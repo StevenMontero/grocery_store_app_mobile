7c98e8ce566cab0e4d22ca700b148a781ae598bc
Steven Montero <steven.monteromunoz@ucrso.info>
2020-09-09 22:04:09 -0600

    se agrega el choselogin y la libreria de idiomas

---
+++ lib/library/language_library/easy_localization.dart
@@ -0,0 +1,4 @@
+library easy_localization;
+
+export 'package:proyecto_sistemas/library/language_library/easy_localization_provider.dart';
+export 'package:proyecto_sistemas/library/language_library/easy_localization_delegate.dart';
--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,37 @@
 import 'package:flutter/material.dart';
- 
-void main() => runApp(MyApp());
- 
+import 'package:flutter/services.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:proyecto_sistemas/library/language_library/easy_localization.dart';
+import 'package:proyecto_sistemas/pages/LoginAndSignUp/choseLoginOrSignuo_page.dart';
+
+void main() => runApp(EasyLocalization(child: MyApp()));
+
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Material App',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Material App Bar'),
-        ),
-        body: Center(
-          child: Container(
-            child: Text('Hello World'),
-          ),
-        ),
+    var data = EasyLocalizationProvider.of(context).data;
+    //Obliga  a la aplicacion a solo funcionar en portrait
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+    ]);
+    //set color status bar
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle.light.copyWith(
+      statusBarColor: Colors.transparent, //or set color with: Color(0xFF0000FF)
+    ));
+    return EasyLocalizationProvider(
+      data: data,
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'La Manada petShop',
+        home: ChoseLogin(),
+        localizationsDelegates: [
+          GlobalMaterialLocalizations.delegate,
+          GlobalWidgetsLocalizations.delegate,
+          EasylocaLizationDelegate(locale: data.locale, path: 'assets/language')
+        ],
+        supportedLocales: [Locale('en', 'US')],
+        locale: data.savedLocale,
       ),
     );
   }
